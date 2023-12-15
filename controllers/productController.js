d72836adb62e3a9c7567f0d8b9bdfb7624ce58f4
Malith Wijekoon <malithcw89@gmail.com>
2023-12-16 00:13:03 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -61,8 +61,41 @@ const getaProduct = AsyncHandler(async (req, res) => {
 
 const getAllProducts = AsyncHandler(async (req, res) => {
   try {
-    const getproducts = await product.find();
-    res.json(getproducts);
+    //Filltering
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let query = product.find(JSON.parse(queryStr));
+    //Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //Pagination
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const productCount = await product.countDocuments();
+      if (skip >= productCount) throw new Error('This page does not exist');
+    }
+
+    const Product = await query;
+    res.json(Product);
   } catch (error) {
     throw new Error(error);
   }
